674be3db60cc4915289f923809d0350cf8130899
李招利 <lizhaoli@huahanyun.com>
2018-01-09 08:59:33 +0800

    增加渐变色 CAGradientLayer

--- CoreAnimations.playground/Sources/Animations.swift
+++ CoreAnimations.playground/Sources/Animations.swift
@@ -275,13 +275,46 @@ extension AnimationsMaker where Layer : CAShapeLayer {
 }
 
 
+extension AnimationsMaker where Layer : CAGradientLayer {
+    
+    /* The array of CGColorRef objects defining the color of each gradient
+     * stop. Defaults to nil. Animatable. */
+    public var colors:AnimationMaker<Layer, [CGColor]> {
+        return AnimationMaker<Layer, [CGColor]>(maker:self, keyPath:"colors")
+    }
+    
+    /* An optional array of NSNumber objects defining the location of each
+     * gradient stop as a value in the range [0,1]. The values must be
+     * monotonically increasing. If a nil array is given, the stops are
+     * assumed to spread uniformly across the [0,1] range. When rendered,
+     * the colors are mapped to the output colorspace before being
+     * interpolated. Defaults to nil. Animatable. */
+    public var locations:AnimationMaker<Layer, [NSNumber]> {
+        return AnimationMaker<Layer, [NSNumber]>(maker:self, keyPath:"locations")
+    }
+    
+    /* The start and end points of the gradient when drawn into the layer's
+     * coordinate space. The start point corresponds to the first gradient
+     * stop, the end point to the last gradient stop. Both points are
+     * defined in a unit coordinate space that is then mapped to the
+     * layer's bounds rectangle when drawn. (I.e. [0,0] is the bottom-left
+     * corner of the layer, [1,1] is the top-right corner.) The default values
+     * are [.5,0] and [.5,1] respectively. Both are animatable. */
+    public var startPoint:AnimationMaker<Layer, CGPoint> {
+        return AnimationMaker<Layer, CGPoint>(maker:self, keyPath:"startPoint")
+    }
+    
+    public var endPoint:AnimationMaker<Layer, CGPoint> {
+        return AnimationMaker<Layer, CGPoint>(maker:self, keyPath:"endPoint")
+    }
 
-
+}
 
 extension AnimationMaker where Value == CGSize {
     
     /// 对 size 的 width 属性进行动画
     public var width:AnimationMaker<Layer, CGFloat> {
+        
         return AnimationMaker<Layer, CGFloat>(maker:maker, keyPath:"\(keyPath).width")
     }
     
